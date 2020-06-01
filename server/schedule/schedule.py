68493d867a5ac380b9961b0277ffcae7928f682d
ikethecoder <ikethecoder@canzea.com>
2020-06-01 15:38:41 -0700

    Fix schedule

--- server/schedule/schedule.py
+++ server/schedule/schedule.py
@@ -51,6 +51,7 @@ def job():
 
 def sched():
     schedule.every().day.at("06:00").do(job)
+    schedule.every().day.at("16:00").do(job)
     #schedule.every(10).seconds.do(job)
     while True:
         schedule.run_pending()
